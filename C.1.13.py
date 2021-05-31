f35127fe8afc8c01edeb4b5dc5ccd58fe8e09a66
NurRahmawatiSubuh <58314157+NurRahmawatiSubuh@users.noreply.github.com>
2021-05-31 22:50:11 +0800

    Add files via upload

---
+++ C.1.13.py
@@ -0,0 +1,9 @@
+def reverse_list_of_integers(list):
+    reversed_array=[]
+    for i in range(0, len(list)):
+        reversed_array.append(list[len(list)-i-1]) 
+    return reversed_array
+print(reverse_list_of_integers([1,2,3,4,5,6,7,8,9,10]))
+
+# append mengurutkan dari belakang
+# prepend mengurutkan dari depan

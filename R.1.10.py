3dbfbdc88d5acd10d587e759adf3399932aa45d7
NurRahmawatiSubuh <58314157+NurRahmawatiSubuh@users.noreply.github.com>
2021-05-31 21:50:28 +0800

    Add files via upload

---
+++ R.1.10.py
@@ -0,0 +1,2 @@
+for i in range ( 8,-10,-2): #nilai awal,nilai akhir,nilai kelipatan
+    print (i);
---
+++ R.1.9.py
@@ -0,0 +1,4 @@
+for i in range ( 50,90,10): # (nilai awal,nilai akhir, nilai kelipatan
+    print (i);
+
+
